7355d2fa2c2ab734b28752fdb4ed69fd5f0c957f
mkbabb <mkbabb@users.noreply.github.com>
2024-07-19 07:24:59 +0000

    deploy: f7c8869e2e366c624e7a0c82534b9ee09db1983e

--- demo/@/components/custom/color-picker/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ColorPicker } from "./ColorPicker.vue";
-export { default as ColorNutritionLabel } from "./ColorNutritionLabel.vue";
--- demo/@/components/custom/katex/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Katex } from "./Katex.vue";
